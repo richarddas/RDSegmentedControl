1b74da5d52f879a64f3622a6ebf2cc6eb7246af1
Richard Das <richard@richarddas.com>
2013-05-07 14:52:12 +0100

    Initial Commit

---
+++ RDSegmentedControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RDSegmentedControl
+//
+//  Created by Richard Das on 7/5/13.
+//  Copyright (c) 2013 RNA Productions, Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ RDSegmentedControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RDSegmentedControl
+//
+//  Created by Richard Das on 7/5/13.
+//  Copyright (c) 2013 RNA Productions, Ltd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
